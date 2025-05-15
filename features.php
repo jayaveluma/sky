704fc26b22076c172350af9e67d93f444866de3f
jayaveluma <165153930+jayaveluma@users.noreply.github.com>
2025-05-15 14:00:37 +0530

    new files

---
+++ features.php
@@ -0,0 +1,233 @@
+<?php include('header.php');?>
+<section class="section-allbanner">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="banner-contents">
+          
+            <div class="title">
+                <h1>All the Essential Features You Need for <span>Powerful Cold Email Infrastructure</span></h1>
+            </div>
+            <div class="desc desfeat">
+                SkySenders provides a complete suite of tools to optimize your email deliverability, automate outreach, and monitor performance in real time. From domain setup to advanced 
+                analytics, get everything you need in one seamless platform.
+            </div>
+            <div class="btn-cont">
+              <a class="btn" href="https://app.skysenders.ai/signup">Get started</a>
+            </div>
+           
+          </div>
+  
+         
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="section-allfeature">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="title">
+                    <h2>Effortless Email Infrastructure Management</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="allfeature-card">
+                    <div class="icon">
+                        <img src="img/icon2.svg" alt="">
+                    </div>
+                    <div class="tile">
+                        <h3>One-Click Domain Setup</h3>
+                    </div>
+                    <div class="desc">
+                        Effortlessly search, register, & purchase
+                        the perfect domain for your business 
+                        with automated DKIM, SPF, and DMARC
+                        setup for enhanced security and 
+                        deliverability.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="allfeature-card">
+                    <div class="icon">
+                        <img src="img/icon1.svg" alt="">
+                    </div>
+                    <div class="tile">
+                        <h3>Dedicated Server per Domain</h3>
+                    </div>
+                    <div class="desc">
+                        Each domain and its mailboxes are 
+                        hosted on a dedicated server, 
+                        ensuring higher reputation and
+                        better deliverability rates.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="allfeature-card">
+                    <div class="icon">
+                        <img src="img/icon3.svg" alt="">
+                    </div>
+                    <div class="tile">
+                        <h3>Add or Remove Mailboxes</h3>
+                    </div>
+                    <div class="desc">
+                        Users can easily add or remove 
+                        mailboxes based on domain health, 
+                        mailbox performance, and usage.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+  </section>
+
+  <section class="section-allfeature allf2">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="title">
+                    <h2>Advanced Analytics & Performance Tracking</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="allfeature-card">
+                    <div class="icon">
+                        <img src="img/icon4.svg" alt="">
+                    </div>
+                    <div class="tile">
+                       <h3> Detailed Analytics</h3>
+                    </div>
+                    <div class="desc">
+                        A comprehensive analytics dashboard 
+                        displaying detailed insights into domain 
+                        and mailbox performance. Helps users 
+                        monitor sending health, bounces, 
+                        spam rates, and overall effectiveness.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="allfeature-card">
+                    <div class="icon">
+                        <img src="img/icon5.svg" alt="">
+                    </div>
+                    <div class="tile">
+                        <h3>Integration & Auto Push</h3>
+                    </div>
+                    <div class="desc">
+                        Supports four major integrations for 
+                        seamless auto-push. Users can enable 
+                        auto-push at the time of domain 
+                        purchase, and we’ll automatically sync 
+                        mailboxes using their API key.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="allfeature-card">
+                    <div class="icon">
+                        <img src="img/icon6.svg" alt="">
+                    </div>
+                    <div class="tile">
+                       <h3> CSV Exports</h3>
+                    </div>
+                    <div class="desc">
+                        Supports eight cold email platforms for 
+                        seamless export/import without 
+                        requiring CSV modifications. No need 
+                        to edit headers – data is automatically 
+                        formatted for each platform.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+  </section>
+
+  <section class="section-allfeature allf2">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="title">
+                    <h2>Customization & Tracking</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="allfeature-card">
+                    <div class="icon">
+                        <img src="img/icon2.svg" alt="">
+                    </div>
+                    <div class="tile">
+                       <h3> Domain Forwarding Setup</h3>
+                    </div>
+                    <div class="desc">
+                        Automatically sets up a custom tracking 
+                        URL when a domain is created, with an 
+                        option to edit it later. Helps improve 
+                        deliverability rates by providing unique 
+                        tracking links.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="allfeature-card">
+                    <div class="icon">
+                        <img src="img/icon7.svg" alt="">
+                    </div>
+                    <div class="tile">
+                        <h3>Custom Tracking URL Setup</h3>
+                    </div>
+                    <div class="desc">
+                        Automatically sets up a custom tracking 
+                        URL when a domain is created, with an 
+                        option to edit it later. Helps improve 
+                        deliverability rates by providing unique 
+                        tracking links.
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="allfeature-card">
+                    <div class="icon">
+                        <img src="img/icon8.svg" alt="">
+                    </div>
+                    <div class="tile">
+                        <h3>Global Inbox</h3>
+                    </div>
+                    <div class="desc">
+                        A unified inbox to view sent, bounced, 
+                        and important emails across all 
+                        mailboxes. Advanced filters to 
+                        categorize emails by domain or 
+                        mailbox.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+  </section>
+
+  <?php include('testimonials.php');?>
+<?php include('footer.php');?>
